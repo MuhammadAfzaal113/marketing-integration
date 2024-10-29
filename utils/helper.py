22c205ac5fffeeaf9e5e03a23ff7cd27badded60
Arbab Sabir <arbab.sabir@sparkai.ca>
2024-10-29 11:37:52 +0500

    move method into helper file

---
+++ utils/helper.py
@@ -0,0 +1,18 @@
+
+
+def json_reader(json_data, key):
+    if isinstance(json_data, dict):
+        if key in json_data:
+            return json_data[key]
+        else:
+            for value in json_data.values():
+                result = json_reader(value, key)
+                if result is not None:
+                    return result
+    elif isinstance(json_data, list):
+        for item in json_data:
+            result = json_reader(item, key)
+            if result is not None:
+                return result
+
+    return None
