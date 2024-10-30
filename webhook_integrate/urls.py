e1af3bd2b3350f3c678f7d28f0425c1367b225f8
Arbab Sabir <arbab.sabir@sparkai.ca>
2024-10-30 10:58:41 +0500

    set url for api's

--- webhook_integrate/urls.py
+++ webhook_integrate/urls.py
@@ -1,9 +1,16 @@
 
-from django.urls import re_path, path
+from django.urls import path
 from webhook_integrate.views import *
 
 
 urlpatterns = [
     path('generate-url', generate_url),
     path('get-webhook-list', get_webhook_list),
+    path('create-filter', create_filter),
+    path('get-webhook-list', get_webhook_list),
+    path('create-action', create_action),
+    path('update-action', update_action),
+    path('update-filter', update_filter),
+    path('delete-action', delete_action),
+    path('delete-filter', delete_filter),
 ]
