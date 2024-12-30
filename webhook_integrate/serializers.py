3fbc2991a0708fd675008d5940d853f085254668
Arbab Sabir <Arbabsabir336@gmail.com>
2024-12-30 23:42:52 +0500

    add new serializer

--- webhook_integrate/serializers.py
+++ webhook_integrate/serializers.py
@@ -1,37 +1,14 @@
-from webhook_integrate.models import Shop, Webhook, WebhookFilter, WebhookAction, RequestData
+from webhook_integrate.models import *
 from rest_framework import serializers
 
 
-
-class WebhookFilterSerializer(serializers.ModelSerializer):
+class ShopSerializer(serializers.ModelSerializer):
     class Meta:
-        model = WebhookFilter
-        fields = ['id', 'key', 'value', 'operator', 'is_and', 'is_or']
+        model = Shop
+        fields = ['id', 'shop_name', 'api_key']
         
-class WebhookActionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = WebhookAction
-        fields = ['id','email', 'phone', 'first_name', 'last_name', 'creation_date', 'total_cost', 'is_paid', 'is_invoice', 'source', 'customFields']
-
-
-class WebhookDetailsSerializer(serializers.ModelSerializer):
-    filters = serializers.SerializerMethodField(read_only=True)
-    actions = serializers.SerializerMethodField(read_only=True)
-    
-    def get_filters(self, obj):
-        filters = WebhookFilter.objects.filter(webhook=obj)
-        return WebhookFilterSerializer(filters, many=True).data
-    
-    def get_actions(self, obj):
-        actions = WebhookAction.objects.filter(webhook=obj)
-        return WebhookActionSerializer(actions, many=True).data
-    
+class WebhookSerializer(serializers.ModelSerializer):
+    shop = serializers.StringRelatedField(source='shop.shop_name')
     class Meta:
         model = Webhook
-        fields = ['id', 'webhook_url', 'filters', 'actions']
-        
-
-class RequestDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RequestData
-        fields = ['id', 'data', 'created_at',]
+        fields = ['id', 'shop', 'webhook_name', 'webhook_url', 'is_filter']
