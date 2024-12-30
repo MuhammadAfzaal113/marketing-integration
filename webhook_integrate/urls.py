61f274bf345800089313beb2fbc79753ac8d9375
Arbab Sabir <Arbabsabir336@gmail.com>
2024-12-30 23:43:01 +0500

    ad api url

--- webhook_integrate/urls.py
+++ webhook_integrate/urls.py
@@ -1,8 +1,24 @@
-from django.urls import path, re_path
-from .views import create_webhook, create_webhook_v2
+from django.urls import path
+from .views import *
 
 
 urlpatterns = [
     path('admin/generate_webhook/', create_webhook, name='generate_webhook_url'),
     path('admin/generate_webhook_v2/', create_webhook_v2, name='generate_webhook_v2_url'),
+    
+    #webhook_url
+    path('generate-webhook-url-v1', generate_webhook_v1, name='generate_webhook_url'),
+    path('generate-webhook-url-v2', generate_webhook_v2, name='generate_webhook_url_v2'),
+    
+    #shop 
+    path('shop/create', create_shop, name='create_shop'),
+    path('shop/update', update_shop, name='update_shop'),
+    path('shop/delete', delete_shop, name='delete_shop'),
+    path('get-shop-list', get_shop, name='get_shop_list'),
+    
+    #webhook
+    path('webhook/create', create_webhook, name='create_webhook'),
+    path('webhook/update', update_webhook, name='update_webhook'),
+    path('webhook/delete', delete_webhook, name='delete_webhook'),
+    path('get-webhook-list', get_webhook, name='get_webhook_list'),
 ]
