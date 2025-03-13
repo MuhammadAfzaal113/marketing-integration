21b193c720b180f08691690b556a06660db450e3
Arbab Sabir <Arbabsabir336@gmail.com>
2025-03-13 13:05:04 +0500

    add FilterSerializer and include webhook_filters in WebhookSerializer

--- webhook_integrate/serializers.py
+++ webhook_integrate/serializers.py
@@ -7,8 +7,18 @@ class ShopSerializer(serializers.ModelSerializer):
         model = Shop
         fields = ['id', 'shop_name', 'api_key']
         
+class FilterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'tag_name', 'tag_id']
+        
 class WebhookSerializer(serializers.ModelSerializer):
     shop = serializers.StringRelatedField(source='shop.shop_name')
+    webhook_filters = serializers.SerializerMethodField(read_only=True)
+    
+    def get_filter(self, obj):
+        return FilterSerializer(Tag.objects.filter(Webhook_id=obj.id), many=True).data
+        
     class Meta:
         model = Webhook
-        fields = ['id', 'shop', 'webhook_name', 'webhook_url', 'is_filter']
+        fields = ['id', 'shop', 'webhook_name', 'webhook_url', 'is_filter', 'webhook_filters']
