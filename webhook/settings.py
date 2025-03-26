3c728739a5f3a406ac0b109b3398a37445699a86
Arbab Sabir <Arbabsabir336@gmail.com>
2025-03-27 02:23:28 +0500

    refactor CORS settings by commenting out method and header lists

--- webhook/settings.py
+++ webhook/settings.py
@@ -160,27 +160,27 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOW_METHODS = [
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
-]
+# CORS_ALLOW_METHODS = [
+#     'DELETE',
+#     'GET',
+#     'OPTIONS',
+#     'PATCH',
+#     'POST',
+#     'PUT',
+# ]
 
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-    'Connection',
-    'Accept-Encoding',
-    'user-agent',
-    'Host',
-]
+# CORS_ALLOW_HEADERS = [
+#     'accept',
+#     'accept-encoding',
+#     'authorization',
+#     'content-type',
+#     'dnt',
+#     'origin',
+#     'user-agent',
+#     'x-csrftoken',
+#     'x-requested-with',
+#     'Connection',
+#     'Accept-Encoding',
+#     'user-agent',
+#     'Host',
+# ]
