b968b17452125a6f6a28698189dc87f08b0bc44d
Arbab Sabir <Arbabsabir336@gmail.com>
2025-04-24 13:33:19 +0500

    Update headers in get_customer command for improved API request handling

--- webhook_integrate/management/commands/get_customer.py
+++ webhook_integrate/management/commands/get_customer.py
@@ -12,19 +12,19 @@ class Command(BaseCommand):
             headers = {
                 'accept': 'application/json',
                 'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-                'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaWQiOiI2MWY4NDExZmZlNjQ1ZmE5MzQwMTUzZjEiLCJpZCI6IjYxZjg0MTBkNTE1ZmYzNDExYzU4MjRlMyIsImxpZCI6IjYxZjg0MTFmZmU2NDVmYTkzNDAxNTNmMSIsInAiOiJ3ZWIiLCJyaWQiOiJ1YzEiLCJzYWQiOjAsInNpZCI6IjVkMGE5Y2JkZDg4YmZlNTEiLCJ0Y2lkIjoiNjFmODQxMWZmZTY0NWZhOTM0MDE1M2YxIiwidHVpZCI6IjYxZjg0MTBkNTE1ZmYzNDExYzU4MjRlMyIsImRhdGFTaGFyaW5nIjpmYWxzZSwiaGFzSHEiOmZhbHNlLCJvbmIiOjcsInBheSI6NiwiYXVkIjoic2hvcCIsImlzcyI6Imh0dHBzOi8vYXBpLnNob3Btb25rZXkuY2xvdWQiLCJpYXQiOjE3MzQwODg0NDMsImV4cCI6MTczNDE3NDg0M30.-ksMyOipgaUU8S4rw_QYF5cldx1QyiFlvoKXHHggor4',
+                'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaWQiOiI2MWY4NDExZmZlNjQ1ZmE5MzQwMTUzZjEiLCJpZCI6IjYxZjg0MTBkNTE1ZmYzNDExYzU4MjRlMyIsImxpZCI6IjYxZjg0MTFmZmU2NDVmYTkzNDAxNTNmMSIsInAiOiJ3ZWIiLCJyaWQiOiJ1YzEiLCJzYWQiOjAsInNpZCI6Ijc1OTNlY2I5OWJlOGJjMWUiLCJ0Y2lkIjoiNjFmODQxMWZmZTY0NWZhOTM0MDE1M2YxIiwiZGF0YVNoYXJpbmciOmZhbHNlLCJoYXNIcSI6ZmFsc2UsIm9uYiI6NywicGF5Ijo2LCJhdWQiOiJzaG9wIiwiaXNzIjoiaHR0cHM6Ly9hcGkuc2hvcG1vbmtleS5jbG91ZCIsImlhdCI6MTc0NTQ4MzE3NiwiZXhwIjoxNzQ1NTY5NTc2fQ.6C1vSm2ZOGVbZYrHsJ-zjUhQ6ZfY30H-94pgXkAFMsE',
                 'cache-control': 'max-age=0',
                 'content-type': 'application/json',
-                'cookie': 'intercom-id-uq2tacb0=e956fa3e-8f16-4522-bea6-37f463d9fd08; intercom-device-id-uq2tacb0=964aa623-2a5d-4ca8-bf49-6b7834dac173; intercom-session-uq2tacb0=OW1GOU9SZnJYZWtUa2lVUnIvQmpybUFVVHE2TFQvQ0FlWkFCcHo0T0J4OGt1YjhmVnZqdEU5bjZCNE50czBGdC0tbzdObENmTi8xYU82K0NnNlJSa2c5UT09--488688099d48fe0be8a4542f44dcd7fc5c2834a4; director-session-id=5d0a9cbdd88bfe51; director-session-id=5d0a9cbdd88bfe51',
+                'cookie': 'intercom-id-uq2tacb0=78d745da-4151-4053-a6da-1a21fef7e851; intercom-device-id-uq2tacb0=f8d78c97-1d9d-4741-be0a-17943c89540c; director-session-id=7593ecb99be8bc1e; intercom-session-uq2tacb0=dS9NTzRRWHgyYXczaXFuOHZVaFRvajYwazFlVzlJR3VLM1E1T3lkM0F4TUk1WUp1d3NMaW9lWVpaUUR1UGRGbmhXa3F6Z2F6NTRpTzZ3MFZiNDA3cWRBb1c1aFgxZlVpRWdoS000cC9KVWs9LS1obFQ3QytxOHp4LzJGMXNaMmJsVnlnPT0=--7ee2ec88bf05f35e9456d1312dfd51f651a0fdcc',
                 'origin': 'https://app.shopmonkey.cloud',
                 'priority': 'u=1, i',
-                'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+                'sec-ch-ua': '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
                 'sec-ch-ua-mobile': '?0',
                 'sec-ch-ua-platform': '"macOS"',
                 'sec-fetch-dest': 'empty',
                 'sec-fetch-mode': 'cors',
                 'sec-fetch-site': 'same-site',
-                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
                 'x-request-location': '/lists/customers'
             }
 
