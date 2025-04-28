0e843e2b2425f333f202e5ff6898e0c0f44b0466
Arbab Sabir <Arbabsabir336@gmail.com>
2025-04-28 22:59:14 +0500

    Add webhook field to DataBaseLogsSerializer for read-only access to webhook URL

--- dblogs/serializers.py
+++ dblogs/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import DataBaseLogs
 
 class DataBaseLogsSerializer(serializers.ModelSerializer):
+    webhook = serializers.CharField(source='webhook.webhook_url', read_only=True)
     class Meta:
         model = DataBaseLogs
         fields = '__all__'
