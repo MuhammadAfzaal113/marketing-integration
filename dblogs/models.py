d4b0ab37cb6663b160844d96b57e5c5488ca6e0a
Arbab Sabir <Arbabsabir336@gmail.com>
2025-04-28 22:59:08 +0500

    Add ForeignKey relationship to Webhook and update error field to allow null values in DataBaseLogs model

--- dblogs/models.py
+++ dblogs/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from utils.abstract_models import CommonFields
 from dblogs.enums import *
+from webhook_integrate.models import Webhook
 
 # Create your models here.
 
 class DataBaseLogs(CommonFields):
+    webhook = models.ForeignKey(Webhook, on_delete=models.CASCADE, null=True, blank=True)
     description = models.TextField()
-    error = models.CharField(max_length=255)
+    error = models.CharField(max_length=255, null=True, blank=True)
     webhook_version = models.CharField(max_length=50, choices=WEBHOOK_VERSION_CHOICES)
     level = models.CharField(max_length=50, choices=LEVEL_TYPE_CHOICES)
     action = models.CharField(max_length=50, choices=ACTION_TYPE_CHOICES)
